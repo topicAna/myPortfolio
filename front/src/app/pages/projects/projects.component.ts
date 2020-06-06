1c7779a452cc20176dcac4380d09d571d0c55159
lattara <atopic.ie@gmail.com>
2020-06-06 19:14:08 +0200

    backend

--- front/src/app/pages/projects/projects.component.ts
+++ front/src/app/pages/projects/projects.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from '../../services/projects.service';
+import { Project } from 'src/app/models/project.model';
 
 @Component({
   selector: 'app-projects',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private projectsService: ProjectsService ) { }
+  projects: Project[] = [];
 
   ngOnInit(): void {
-  }
-
+    this.projectsService.getProjects().subscribe(response => {
+      this.projects = response;
+      console.log(this.projects);
+    });
+}
 }
--- front/src/app/services/projects.service.ts
+++ front/src/app/services/projects.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-// import {Dishes } from './dishes';
-import {Router} from '@angular/router';
+import { Project } from '../models/project.model';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -12,32 +12,30 @@ export class ProjectsService {
   private baseUrl = 'http://localhost:3000';
 
   constructor(private http: HttpClient, private router: Router) {
-
   }
 
-putProject(project: Project, id: number): Observable<any> {
-    console.log(dishes)
-    const putDishesurl = `${this.baseUrl}/dishes/${id}`;
-    console.log(putDishesurl);
-    return this.http.put(putDishesurl, dishes);
-}
+  putProject(project: Project, id: number): Observable<any> {
+    console.log(project);
+    const putProjectsURL = `${this.baseUrl}/projects/${id}`;
+    console.log(putProjectsURL);
+    return this.http.put(putProjectsURL, project);
+  }
 
-postDishes(dishes: Dishes,): Observable<any> {
-    return this.http.post(`${this.baseUrl}/dishes`,dishes);
-}
+  postProject(project: Project): Observable<any> {
+    return this.http.post(`${this.baseUrl}/projects`, project);
+  }
 
-getDishes(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/dishes`);
-}
+  getProjects(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/projects`);
+  }
 
+  deleteProject(id: number): Observable<any> {
+    const url = `${this.baseUrl}/project/${id}`;
+    return this.http.delete(url);
+  }
 
-deleteDishes(id: number): Observable<any> {
-    const url = `${this.baseUrl}/dishes/${id}`;
-    return this.http.delete(url)
-}
-
-getDishesById(id: number): Observable<any> {
-    return this.http.get(`${this.baseUrl}/dishes/${id}`);
-}
+  getProjectById(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/project/${id}`);
+  }
 
 }
