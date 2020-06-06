360751d6dc2447ac852e906467c030011c83415f
lattara <atopic.ie@gmail.com>
2020-06-06 19:15:30 +0200

    backedn implementation

--- back/loaders/index.ts
+++
@@ -1,9 +0,0 @@
-import expressLoader from './express';
-import { Application } from 'express';
-
-export default async (app: Application ) => {
-  await expressLoader(app);
-  console.log('Express Intialized');
-
-  // Rajouter le loader MySQL
-};
