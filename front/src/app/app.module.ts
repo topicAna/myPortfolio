965cec165522a0b817cdf4c9ac502206f058772c
lattara <atopic.ie@gmail.com>
2020-06-21 12:11:09 +0200

    setting up texts, position, margins

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonComponent } from './components/button/button.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { TitleComponent } from './components/title/title.component';
@@ -35,7 +34,6 @@ import {CdkTableModule} from '@angular/cdk/table';
   declarations: [
     AppComponent,
     ButtonComponent,
-    NavbarComponent,
     FooterComponent,
     LandingComponent,
     TitleComponent,
