a0e4db5276007a3281b2d2c0c24ff908e77d7996
lattara <atopic.ie@gmail.com>
2020-06-23 22:50:34 +0200

    routing on dashboard

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -4,21 +4,26 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { BioComponent } from './pages/bio/bio.component';
 import { MainComponent } from './pages/main/main.component';
 import { AdminDashboardComponent } from './dashboard/admin-dashboard/admin-dashboard.component';
+import { BioDashboardComponent } from './dashboard/bio-dashboard/bio-dashboard.component';
+import { ProjectsDashboardComponent } from './dashboard/projects-dashboard/projects-dashboard.component';
+import { MiscDashboardComponent } from './dashboard/misc-dashboard/misc-dashboard.component';
 
 
 const routes: Routes = [
   {path: '', component: MainComponent},
-  {path: 'dashboard', component: AdminDashboardComponent, pathMatch: 'full',
-
-    children: [
-      // {path: 'admin-doc', component: AdminDocComponent},
-      // {path: 'home', component: DashboardComponent},
-      // {path: 'informations', component: DashboardInformationsComponent},
-      // {path: 'products', component: DashboardProductsComponent},
-      // {path: 'documents', component: DashboardDocumentsComponent},
-      // {path: 'request', component: DashboardRequestComponent},
-      // {path: 'account', component: DashboardAccountComponent},
-    ],
+  {path: 'dashboard', component: AdminDashboardComponent,
+  children: [
+    {path: 'home', component: ProjectsDashboardComponent},
+    {path: 'bio', component: BioDashboardComponent},
+    {path: 'misc', component: MiscDashboardComponent},
+    // {path: 'fonds-de-credit-multi-emprunteurs', component: FondsMultiEmprunteursComponent},
+    // {path: 'garantie-et-pret-ifcic', component: GarantieEtPretIfcicComponent},
+    // {path: 'panorama-des-marches-financiers', component: MarchesFinanciersComponent},
+    // {path: 'fonds-de-capital-investissement', component: CapitalInvestissementComponent},
+    // {path: 'fonds-ou-dispositifs-specifiques-au-secteur', component: FondsBoisComponent},
+    // {path: 'business-angels', component: BusinessAngelsComponent},
+    // {path: 'les-aides', component: AidesComponent},
+  ],
   },
 ];
 
