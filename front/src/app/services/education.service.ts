c5e11c01469bdf3553905c4eb19d3f02276cb23a
lattara <atopic.ie@gmail.com>
2020-07-28 08:58:14 +0200

    educations implemented;

---
+++ front/src/app/services/education.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { Education } from '../models/education';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class EducationService {
+
+    private baseUrl = 'http://localhost:3000/education';
+
+    constructor(private http: HttpClient, private router: Router) {
+    }
+
+    getEducations(): Observable<any> {
+        return this.http.get(`${this.baseUrl}`);
+    }
+
+    putEducation(education: Education, id: number): Observable<any> {
+        const putEducationURL = `${this.baseUrl}/${id}`;
+        return this.http.put(putEducationURL, education);
+    }
+
+    postEducation(education: Education): Observable<any> {
+        return this.http.post(`${this.baseUrl}`, education);
+    }
+
+    deleteEducation(id: number): Observable<any> {
+        const url = `${this.baseUrl}/${id}`;
+        return this.http.delete(url);
+    }
+
+    getEducationById(id: number): Observable<any> {
+        return this.http.get(`${this.baseUrl}/${id}`);
+    }
+
+}
