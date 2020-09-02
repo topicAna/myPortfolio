5e3063f1cc66f5ce5044a469ef6debc42e6806e6
lattara <atopic.ie@gmail.com>
2020-09-02 09:37:23 +0200

    custom validators

--- front/src/app/guards/custom-validators.ts
+++ front/src/app/guards/custom-validators.ts
@@ -1,7 +1,12 @@
 import { AbstractControl } from '@angular/forms';
 
 export function nameValidator(control: AbstractControl) {
-    if (!control.value.startsWith('lattara') || !control.value.includes('admin')) {
+
+    if (
+    !control.value.startsWith('lattara') ||
+    !control.value.includes('lattara') ||
+    !control.value.startsWith('admin') ||
+    !control.value.includes('admin')) {
         return { nameValid: false };
     }
     return null;
