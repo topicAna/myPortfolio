72fb4d4c11a7a5ee811564963435ced2daecb766
lattara <atopic.ie@gmail.com>
2020-10-08 15:34:26 +0200

    add remove experience implemented

--- front/src/app/dashboard/misc-dashboard/misc-dashboard.component.ts
+++ front/src/app/dashboard/misc-dashboard/misc-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Experience } from 'src/app/models/experience';
+import { ExperienceService } from 'src/app/services/experience.service';
 
 @Component({
   selector: 'app-misc-dashboard',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MiscDashboardComponent implements OnInit {
 
-  constructor() { }
+  experiences: Experience[];
+  newExperience: Experience = new Experience();
+
+  constructor(private experienceService: ExperienceService) { }
+
+  formGroup = new FormGroup({
+    title: new FormControl('', Validators.required),
+    company: new FormControl('', Validators.required),
+    duration: new FormControl('', Validators.required),
+  });
 
   ngOnInit(): void {
+    this.getAllExperiences()
   }
+
+  saveExperience() {
+    this.newExperience.title = this.formGroup.value.title;
+    this.newExperience.company = this.formGroup.value.company;
+    this.newExperience.duration = this.formGroup.value.duration;
+    this.experienceService.postExperience(this.newExperience).subscribe( result => this.experiences.push(result));
+  }
+
+
+  getAllExperiences(){
+    this.experienceService.getExperiences().subscribe(results => {
+      this.experiences = results;
+    }
+    );
+  }
+
+  deleteExperience(id: number) {
+    this.experienceService.deleteExperience(id).subscribe(
+      () => {
+        this.experiences.splice(this.experiences.findIndex(item => item.id === id), 1);
+      }
+    );
+  }
+
+  editExperience(id: number, experience: Experience) {
+    this.formGroup.patchValue(experience);
+  }
+
+
 
 }
